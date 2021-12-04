9cfafff885c93e21b9aa91bd38ff7b9b979ca524
juggler39 <juggler39@gmail.com>
2021-12-05 00:20:01 +0300

    collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => {
-    return (
-        <div>
-            <h1>Hats Page</h1>
-        </div>
-    );
-};
+import ShopPage from './pages/shop/shop.component';
 
 const App = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/hats" element={<HatsPage />} />
+                <Route path="/shop" element={<ShopPage />} />
             </Routes>
         </div>
     );
